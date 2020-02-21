8d871c641b627d14185ad43489a48203a26f4f3e
Greg Benson <benson@usfca.edu>
2020-02-21 01:32:45 +0000

    Added week05_02 examples.

---
+++ week05_02/count_zeros_s.s
@@ -0,0 +1,28 @@
+    .global count_zeros_s
+    .func count_zeros_s
+
+/* int *array => r0
+   int len    => r1
+   int i      => r2
+   int count  => r3
+*/
+    
+count_zeros_s:
+    mov r2, #0
+    mov r3, #0
+count_zeros_loop:
+    cmp r2, r1
+    beq count_zeros_loopend
+    ldr r12, [r0]
+    cmp r12, #0
+    bne count_zeros_notzero
+    add r3, r3, #1 
+count_zeros_notzero:
+    add r2, r2, #1
+    add r0, r0, #4
+    b count_zeros_loop
+count_zeros_loopend:    
+    mov r0, r3
+    bx lr
+    .endfunc
+    
