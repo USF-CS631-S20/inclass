e288b8f0c26b6186e5811ffae8b24f34c51945af
Greg Benson <benson@usfca.edu>
2020-03-26 23:56:06 +0000

    Updated utils.h.

--- project04/armemu_cache/utils.h
+++ project04/armemu_cache/utils.h
@@ -1,6 +1,10 @@
 /* utils.h - Public functions in utils.c */
+#ifndef UTILS_H
+#define UTILS_H
 
 #include <stddef.h>
 
 void *malloc_helper(size_t bytes, char *function_name);
+
+#endif /* UTILS_H */
 
